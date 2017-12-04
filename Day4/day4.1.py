8c1e94b29d67364d259469f1ad8c653493264ebb
Sam Ehret <ehret.sam@yahoo.com>
2017-12-04 16:42:31 -0600

    In Progress

---
+++ Day4/day4.1.py
@@ -0,0 +1,21 @@
+with open('passcode.txt', 'r') as myfile:
+    passphrases = myfile.readlines()
+passcode = []
+check = []
+checksum = 0
+
+for line in passphrases:
+    passcode.append(line)
+
+for val in passcode:
+    line = list(val.split())
+    for word in line:
+        if word not in check:
+            check.append(word)
+            if line[:] == check[:]:
+                checksum = checksum + 1
+                del check[:]
+        else:
+            del check[:]
+
+print(checksum)
