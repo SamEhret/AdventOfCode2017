c423f7d5c5aefc288c2bbdc77af336662030f7a8
Sam Ehret <ehret.sam@yahoo.com>
2017-12-10 11:23:07 -0600

    Finished Day 6 Problems

---
+++ Day6/day6.2.py
@@ -0,0 +1,39 @@
+import sys
+
+nums = []
+f = open('Day6List.txt', 'rU')
+f = f.read()
+f = f.split()
+for val in f:
+    nums.append(int(val))
+
+track = []
+track.append(str(nums))
+n = nums.index(max(nums)) + 1
+count = max(nums)
+nums[n - 1] = 0
+
+while len(track) < 5000:
+    if count > 0:
+        if n == len(nums) - 1:
+            nums[n] = nums[n] + 1
+            count = count - 1
+            n = 0
+        else:
+            nums[n] = nums[n] + 1
+            count = count - 1
+            n = n + 1
+    elif count == 0:
+        for val in track:
+            if val == str(nums):
+                print(len(track) - track.index(val))
+                sys.exit()
+        else:
+            track.append(str(nums))
+            n = nums.index(max(nums))
+            count = max(nums)
+            nums[n] = 0
+            if n == len(nums) - 1:
+                n = 0
+            else:
+                n = n + 1
