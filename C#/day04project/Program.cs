adb66130dbfc8af5379cd1eed6cd1460d84f411c
Sam Ehret <ehret.sam@yahoo.com>
2018-05-12 11:01:33 -0500

    added day 4 solution

---
+++ C#/day04project/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace day04project
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Day04.Run();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/day04project/day04.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace day04project
+{
+    public static class Day04
+    {
+        public static void Run()
+        {
+            var line = "";
+            var input = new List<string>();
+            var file = new StreamReader(@"day04input.txt");
+            while((line = file.ReadLine()) != null)  
+            {  
+                input.Add(line);
+            }
+
+            Part01(input);
+            Part02(input);
+        }
+
+        private static void Part01(IEnumerable<string> input)
+        {
+            var validCount = 0;
+            var checkList = new List<string>();
+            
+            foreach (var line in input)
+            {
+                var tempLine = line.Split(null);
+                checkList.Clear();
+                
+                foreach (var item in tempLine)
+                {
+                    if (checkList.Contains(item)) continue;
+                    checkList.Add(item);
+                    
+                    if (!tempLine.SequenceEqual(checkList)) continue;
+                    validCount += 1;
+                }
+            }
+            Console.WriteLine($"Part 1 Answer: {validCount}");
+        }
+
+        private static void Part02(IEnumerable<string> input)
+        {
+            var validCount = 0;
+            var checkList = new List<string>();
+            
+            foreach (var line in input)
+            {
+                var tempLine = line.Split(null);
+                checkList.Clear();
+                
+                foreach (var item in tempLine)
+                {
+                    var tempItem = new string (item.OrderBy(c => c).ToArray());
+                    
+                    if (checkList.Contains(tempItem)) continue;
+                    checkList.Add(tempItem);
+                    
+                    if (tempLine.Length != checkList.Count) continue;
+                    validCount += 1;
+                }
+            }
+            Console.WriteLine($"Part 2 Answer: {validCount}");
+        }
+    }
+}
